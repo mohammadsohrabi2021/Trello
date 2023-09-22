d167f27a493f4816aeaccd92e66ffb04bde3f86f
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-09-22 20:08:50 +0330

    refactor app.js done

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import Layout from "@/components/layout/Layout";
+import "@/styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+   <SessionProvider session={pageProps.session}>
+     <Layout>
+      <Component {...pageProps} />
+    </Layout>
+   </SessionProvider>
+  );
 }
