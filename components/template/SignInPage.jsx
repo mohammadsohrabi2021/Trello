614020811dcc6bd2aa776676c72e0c3eab6813c4
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-09-29 16:32:42 +0330

    create component SignInPage done

---
+++ components/template/SignInPage.jsx
@@ -0,0 +1,49 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect } from "react";
+function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const { status } = useSession();
+
+  useEffect(() => {
+    if (status === "authenticated") router.replace("/");
+  }, [status]);
+
+  const loginHandler = async () => {
+    const res = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (!res.error) router.push("/");
+  };
+  return (
+    <div className="signin-form">
+      <h3>Login Form</h3>
+      <input
+        type="text"
+        placeholder="plase enter email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="plase enter passwrd"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={loginHandler}>Login</button>
+      <div>
+        <p>Create an account?</p>
+        <Link href={"/signup"}>Sign Up</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignInPage;
