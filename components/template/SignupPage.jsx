de1772f1e256b8e33b84e9f430a6e8784bae1a4e
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-09-29 16:33:33 +0330

    create component SignupPage done

---
+++ components/template/SignupPage.jsx
@@ -0,0 +1,52 @@
+import { useSession } from "next-auth/react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useEffect } from "react";
+
+function SignupPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const {status} =useSession()
+  
+  useEffect(() => {
+    if (status === "authenticated") router.replace("/");
+  }, [status]);
+
+  const signupHandler = async () => {
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: { "Content-Type": "application/json" },
+    });
+    const data = await res.json();
+    console.log(data);
+    if (data.status === "success") router.push("/signin");
+  };
+
+  return (
+    <div className="signin-form">
+      <h3>Registration Form</h3>
+      <input
+        type="text"
+        placeholder="plase enter email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="plase enter passwrd"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={signupHandler}>Register</button>
+      <div>
+        <p>Have an account?</p>
+        <Link href={"/signin"}>Sign In</Link>
+      </div>
+    </div>
+  );
+}
+
+export default SignupPage;
