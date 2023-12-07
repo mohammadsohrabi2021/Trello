9e7521302a4fc6c7ff730e75208e5e18dad384a5
Mohammad Sohrabi <mohammadsohrabi141@gmail.com>
2023-12-08 02:39:58 +0330

    refactoring component SignInPage done

--- components/template/SignInPage.jsx
+++ components/template/SignInPage.jsx
@@ -4,9 +4,12 @@ import React, { useState } from "react";
 import { signIn, useSession } from "next-auth/react";
 import { useEffect } from "react";
 import { setCookie } from "nookies";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function SignInPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false); // اضافه کردن وضعیت "loading"
   const router = useRouter();
   const { status } = useSession();
 
@@ -15,36 +18,55 @@ function SignInPage() {
   }, [status]);
 
   const loginHandler = async () => {
-    const res = await signIn("credentials", {
-      email,
-      password,
-      redirect: false,
-    });
-  
-    // setCookie(null, "token", email, { path: "/" });
-    if (!res.error) router.push("/");
+    try {
+      setLoading(true); // شروع فرآیند loading
+
+      const res = await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+      });
+     
+      if (!res.error){
+        toast.success(res.error);
+        router.push("/");
+      }
+      else{
+        toast.error(res.error);
+      }
+    } catch (error) {
+      toast.error("An error occurred. Please try again.");
+    } finally {
+      setLoading(false); // پایان فرآیند loading
+    }
   };
+
   return (
-    <div className="signin-form">
+   <>
+    <div className={"registration-form"}>
       <h3>Login Form</h3>
       <input
         type="text"
-        placeholder="plase enter email"
+        placeholder="Please enter email"
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
       <input
         type="password"
-        placeholder="plase enter passwrd"
+        placeholder="Please enter password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
-      <button onClick={loginHandler}>Login</button>
-      <div>
+      <button onClick={loginHandler} className={"registration-button"}>
+        Login
+      </button>
+      <div className={"have-account"}>
         <p>Create an account?</p>
         <Link href={"/signup"}>Sign Up</Link>
       </div>
     </div>
+    {/* <ToastContainer /> */}
+   </>
   );
 }
 
